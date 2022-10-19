32e46db3b2e56d55e7ca9b22b7dc2c5f38a16170
Hagen <hagen@hyena.gay>
2022-10-19 10:12:49 +0100

    0.1.3

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
 
-version '0.1.2'
+version '0.1.3'
 author 'Reece Stokes <hagen@hyena.gay>'
 description 'FiveM resource for ACE-specific blacklisting.'
 repository 'https://github.com/MrGriefs/dynamicblacklist'
